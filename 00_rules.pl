2c1dcff449efc4cb1ba4aba3ce1046ca954c7bc7
Micael Dias <diasmicaelandre@gmail.com>
2023-03-07 12:35:40 +0000

    Create 00_rules.pl

---
+++ 00_rules.pl
@@ -0,0 +1,31 @@
+% escola(nome_politecnico, escola).
+
+escola(estg, ipp).
+
+% curso(sigla_curso, escola).
+
+curso(lei, estg).
+
+% uc(sigla_curso, sigla_uc, ano).
+
+uc(lei, ai, 3).
+
+% docente(sigla_uc, nome).
+
+docente(ai, ramos).
+
+% inscrito(sigla_uc, aluno).
+
+inscrito(ai, micael).
+
+% quais sao os docentes de uma instituição
+
+docente_esta_na_instituicao(D, I):-docente(UC, D),uc(C, UC, _),curso(C, E),escola(E, I).
+
+% quais sao os docente de uma escola
+
+docente_na_escola(D, E):-docente(UC, D),uc(C, UC, _),curso(C, E).
+
+% há ucs sem inscritos? em que cursos?
+
+sem_incritos(E):-curso(C, E),uc(C, UC,_),incrito(UC, _).
